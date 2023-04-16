5ab1dbe19621e2569b1d843bc05e164947b52955
adielas <adiel.ashrov@gmail.com>
2023-04-16 12:27:26 +0300

    Update MonitorNetworkStateBThread.cpp
    Protocol switches back-forth between Yield/Restore

--- PCC_SBM/src/pcc/rate_control/MonitorNetworkStateBThread.cpp
+++ PCC_SBM/src/pcc/rate_control/MonitorNetworkStateBThread.cpp
@@ -11,7 +11,7 @@ MonitorNetworkStateBThread::~MonitorNetworkStateBThread()
 
 void MonitorNetworkStateBThread::entryPoint()
 {
-     printf("Enter MonitorNetworkStateBThread...\n" );
+    printf("Enter MonitorNetworkStateScenario.\n" );
     
     int id = 1;
     int number_of_changes_in_deviation = 0;
@@ -35,110 +35,78 @@ void MonitorNetworkStateBThread::entryPoint()
         watched.clear();
         blocked.clear();
         watched.append(monitorIntervalEvent); 
-        // printf("MonitorNetworkStateBThread: bSync(none, monitorIntervalEvent, none)...\n");
-        bSync(requested, watched, blocked, "MonitorNetworkStateBThread"); // Stopped here
+        bSync(requested, watched, blocked, "MonitorNetworkStateBThread");
         Event lastEvent = this->lastEvent();
-        // printf("MonitorNetworkStateBThread: lastEvent.monitorIntervalEvent, id: %d, type: %d\n", lastEvent.id(), lastEvent.type());
         
         float rtt_dev = UtilityCalculator::ComputeRttDeviation(lastEvent.monitorInterval());
         rtt_deviation_1 = rtt_deviation_2;
         rtt_deviation_2 = rtt_dev;
         current_utility = lastEvent.monitorInterval()->GetObsUtility();
-
-        // printf("MonitorNetworkStateBThread: last event is monitorIntervalEvent, id: %d, type: %d, rtt_dev_1: %f, rtt_dev_2: %f\n", lastEvent.id(), lastEvent.type(), rtt_deviation_1, rtt_deviation_2);
+        // printf("current_utility: %4.2f\n", current_utility);
 
         if(monitorHotSwitch)
         {
             if(!enterYieldMode)
             {
-                if(numOfIterInRestoreMode >= 2000)
+                if(numOfIterInRestoreMode >= 1000)
                 {
-                    // if(numOfIterInRestoreMode == 5000)
-                    // {
-                    // printf("Start checking RTT deviation only after 5000 iterations\n");
-                    // }
-                    /*
-                    if(checkIncreaseInRttDev(rtt_deviation_1, rtt_deviation_2))
-                    {
-                        ++number_of_changes_in_deviation;
-                    }
-                    else
-                    {
-                        number_of_changes_in_deviation = 0;
-                    }
-                    */
-
                     if(numOfIterInRestoreMode % 5000 == 0)
                     {
                         numOfDecraseInUtilityValue = 0;
-                        printf("numOfDecraseInUtilityValue = 0\n");
                     }
                     else
                     {
-                        if(current_utility < -100000.0)
+                        if(current_utility < 0.0)
                         {   
                             ++numOfDecraseInUtilityValue;
-                            printf("current_utility: (%f), numOfDecraseInUtilityValue: (%d)\n", current_utility, numOfDecraseInUtilityValue);
                         }
                     }
                     
                     if(numOfDecraseInUtilityValue >= 3)
                     {
-                        printf("***MonitorNetworkStateBThread: enterYieldMode...***\n");
+                        printf("*****MonitorNetworkStateScenario: Yielding throughput...*****\n");
                         enterYieldMode = true;                        
                         numOfIterInYieldMode = 0;
                         numOfIterInRestoreMode = 0;
                         numOfDecraseInUtilityValue = 0;
                         Event monitorIntervalEvent_2(0, id);
-                        Event getNextSendingRateEvent(1, id); // 2 signals updateSendingRate
-                        Event yieldBandwidthEvent(5, id); // 5 signals yield bandwidth event
+                        Event queryNextSendingRateEvent(1, id); // 2 signals updateSendingRate
+                        Event enterYieldEvent(5, id); // 5 signals enterYield event
                         requested.clear();
                         watched.clear();
                         blocked.clear();
-                        requested.append(yieldBandwidthEvent);
+                        requested.append(enterYieldEvent);
                         blocked.append(monitorIntervalEvent_2);
-                        blocked.append(getNextSendingRateEvent);
-                        // printf("MonitorNetworkStateBThread: bSync(yieldBandwidthEvent, none, {monitorIntervalEvent, getNextSendingRateEvent})...\n");
+                        blocked.append(queryNextSendingRateEvent);
                         bSync(requested, watched, blocked, "MonitorNetworkStateBThread");
-                        // printf("MonitorNetworkStateBThread: last event is yieldBandwidthEvent\n");
                     }
                 }
-                // else
-                // {
-                    ++numOfIterInRestoreMode;
-                // }
-
+                ++numOfIterInRestoreMode;
             }
             else
             {
-                if(numOfIterInYieldMode >= 3000)
+                if(numOfIterInYieldMode >= 1000)
                 {
                     enterYieldMode = false;                        
                     numOfIterInYieldMode = 0;
                     numOfIterInRestoreMode = 0;
-                    printf("***MonitorNetworkStateBThread: Restoring throughput...***\n");
-                    Event restoreThroughputEvent(6, id); // 6 signals yield bandwidth event 
+                    printf("*****MonitorNetworkStateScenario: Restoring throughput...*****\n");
+                    Event restoreThroughputEvent(6, id); // 6 signals restore bandwidth event 
                     requested.clear();
                     watched.clear();
-                    blocked.clear(); // Should we block anything?
+                    blocked.clear();
                     requested.append(restoreThroughputEvent);
-                    // printf("MonitorNetworkStateBThread: bSync(restoreThroughputEvent, none, none)...\n"); 
                     bSync(requested, watched, blocked, "MonitorNetworkStateBThread");
-                    // printf("MonitorNetworkStateBThread: last event is restoreThroughputEvent...\n");
                 }
-                // else
-                // {
-                    ++numOfIterInYieldMode;
-                // }
+                ++numOfIterInYieldMode;
             }
         }
 
         ++id;
-        // usleep(100000);
     }
     
     done();
-    printf("Leave MonitorNetworkStateBThread...\n" );
+    printf("Leave MonitorNetworkStateScenario...\n" );
 }
 
 bool MonitorNetworkStateBThread::checkIncreaseInRttDev(float rtt_deviation_1, float rtt_deviation_2)
